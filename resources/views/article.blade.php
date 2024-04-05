71774c608ae5a726062ad32faa5b46015f3b87bf
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2024-04-05 15:03:24 +0100

    done

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -4,7 +4,7 @@
  <head>
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     @vite(['resources/css/app.css', 'resources/js/app.js'])
+     @vite('resources/js/app.js')
      @vite('resources/css/app.css')
      <link rel="icon" href="{{ asset('/images/Ebuka.jpeg') }}" type="image/png">
      <!-- Meta Tags for Search Engines -->

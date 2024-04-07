91772794197670e45dead4823f6ea3106b0bdd8b
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2024-04-07 22:34:21 +0100

    done

---
+++ resources/js/components/pages/admin/adminLogin.vue
@@ -0,0 +1,83 @@
+
+
+<template>
+    <main
+        class="fixed top-0 left-0 w-full h-screen overflow-y-auto bg-[rgb(236,245,255)]"
+    >
+        <div
+            class="card bg-white mx-auto mt-20 p-6 shadow-xl sm:mt-32 sm:w-[30em] sm:max-w-[40em]"
+        >
+            <div class="card-body">
+                <div class="card-title">
+                    <h1
+                        class="text-center text-blue-500 text-2xl sm:text-3xl rounded-badge mx-auto"
+                    >
+                        Lorenzo
+                    </h1>
+                </div>
+                <div class="mx-auto mt-4 flex flex-col items-center">
+                    <div class="flex justify-center items-center">
+                        <span
+                            class="text-lg sm:text-2xl text-blue-500 font-semibold mr-2"
+                            >Sign In</span
+                        >
+                        <span
+                            class="inline-flex bg-blue-50 rounded-full text-sm sm:text-lg p-1 sm:p-3 text-blue-400"
+                        >
+                            <span class="w-max">as a super Admin</span>
+                        </span>
+                    </div>
+                    <span
+                        class="block text-center mt-3 text-[rgb(102,112,133)] text-sm sm:text-base"
+                        >Welcome back to CindyAI. Please signin to access your
+                        account</span
+                    >
+                </div>
+                <div class="mx-auto">
+                    <label class="form-control w-full mt-5 max-w-xs">
+                        <div class="label">
+                            <span class="label-text text-medium font-medium"
+                                >Email Address</span
+                            >
+                        </div>
+                        <input
+                            type="email"
+                            placeholder=""
+                            class="input input-bordered w-full sm:w-80"
+                        />
+                        <small class="text-center text-red-500">
+                            Invalid Email
+                        </small>
+                    </label>
+                    <label class="form-control w-full mt-1 max-w-xs">
+                        <div class="label">
+                            <span class="label-text text-medium font-medium"
+                                >Password</span
+                            >
+                        </div>
+                        <input
+                            type="password"
+                            placeholder=""
+                            class="input input-bordered w-full input-info sm:w-80"
+                        />
+                        <!-- <small class="text-center text-red-500"> hello </small> -->
+                    </label>
+                    <a
+                        href=""
+                        class="p-3 px-6 pt-4 text-white text-center mt-4 bg-blue-500 rounded-btn block w-full sm:w-auto"
+                    >
+                        <span> Sign In </span>
+
+                        <span> Please wait .... </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+.invalid-input {
+    border-color: red; /* Change border color for invalid email */
+}
+</style>

3458fe93cbeb1881a8eed315844658bf2becfa2f
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2024-04-15 23:06:37 +0100

    dn

--- resources/js/components/pages/ArticleComponent.vue
+++ resources/js/components/pages/ArticleComponent.vue
@@ -33,6 +33,23 @@ const articleStore = useEditorStore();
                         >
                     </div>
                 </div>
+
+                <div
+                    class="join grid grid-cols-2 mt-4 space-x-2 self-end max-w-sm w-full" v-if="articleStore.articles.length === 20"
+                >
+                    <button
+                        class="join-item border-blue-500 bg-blue-500 px-4 p-1 text-white"
+                        @click="articleStore.handlePageChange(-1)"
+                    >
+                        Previous page
+                    </button>
+                    <button
+                        class="join-item bg-blue-500 px-4 p-1 text-white border-blue-500"
+                        @click="articleStore.handlePageChange(1)"
+                    >
+                        Next
+                    </button>
+                </div>
             </section>
         </div>
     </section>
@@ -43,7 +60,7 @@ const articleStore = useEditorStore();
     >
         <div class="max-w-screen-lg px-4 py-4 lg:py-8 mx-auto">
             <section class="h-screen">
-                <h1 class="text-4xl text-white"> No posts yet... coming soon </h1>
+                <h1 class="text-4xl text-white">No posts yet... coming soon</h1>
             </section>
         </div>
     </section>
